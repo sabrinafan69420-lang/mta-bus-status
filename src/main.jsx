1af06217130d568e7bcf5eff56034e0a664c6904
fredo :3 <spiii@MacBookAir.home>
2026-06-21 23:27:22 -0400

    force SW update and page reload when new version activates

--- src/main.jsx
+++ src/main.jsx
@@ -30,6 +30,17 @@ createRoot(document.getElementById('root')).render(
 
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", () => {
-    navigator.serviceWorker.register("/sw.js").catch(() => {});
+    navigator.serviceWorker.register("/sw.js").then((reg) => {
+      reg.addEventListener("updatefound", () => {
+        const newWorker = reg.installing;
+        if (newWorker) {
+          newWorker.addEventListener("statechange", () => {
+            if (newWorker.state === "activated") {
+              window.location.reload();
+            }
+          });
+        }
+      });
+    }).catch(() => {});
   });
 }
